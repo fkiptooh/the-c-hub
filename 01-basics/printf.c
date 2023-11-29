cf9cb803f3ac54670919f6586293cd8d6efe913e
fkiptooh <fkiptooh@gmail.com>
2023-11-29 15:10:57 +0300

    creating two statements in different lines using a single printf

---
+++ 01-basics/printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Main execution entry point;
+ * takes no arguments;
+ *
+ * Return: - Has no return value;
+*/
+int main(void)
+{
+	/* printf that prints two lines on the console using \n; */
+	printf("Hello my friend\nhappy to meet you here\n");
+	return (0);
+}
