cac33f39967282d0f52eb9ac07336c9f2f989196
fkiptooh <fkiptooh@gmail.com>
2023-11-29 15:52:25 +0300

    prints two lines and also escape quotation marks

---
+++ 01-basics/print_two_lines.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point;
+ * takes no arguments;
+ *
+ * prints the name in qoutes using the escape character;
+ * Return: - has no return value;
+ */
+int main(void)
+{
+	printf("Hello\nCall me Felix: \"Felix\"\n");
+	return (0);
+}
