d1da0cadb818571fb309082d9798eeafa5ef9348
fkiptooh <fkiptooh@gmail.com>
2023-11-29 14:55:17 +0300

    sending text to output using puts

---
+++ 01-basics/puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Programs entry point;
+ * takes no parameters;
+ *
+ * Return: - Has no return type;
+*/
+int main(void)
+{
+	/* Using puts in place of printf */
+	puts("Thrilled to use puts in place of printf");
+	return (0);
+}
